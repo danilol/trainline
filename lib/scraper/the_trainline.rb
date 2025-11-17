c1ff09880aef6f8254e9cf182ef6a33fb613b624
Danilo Lima <danilo.tech.lima@gmail.com>
2025-11-17 23:44:40 +0000

    Cleanup and refactoring the foundation

--- lib/scraper/the_trainline.rb
+++ lib/scraper/the_trainline.rb
@@ -1,18 +1,103 @@
 module Scraper
   class TheTrainline
-    attr_reader :from, :to, :departure_at
+    attr_reader :from, :to, :departure_at, :results
 
     BASE_URL = "https://www.thetrainline.com"
-    def self.find(from, to, departure_at)
-      puts "From: #{from}"
-      puts "To: #{to}"
-      puts "Departure At: #{departure_at}"
-    end
 
     def initialize(from, to, departure_at)
       @from = from
       @to = to
       @departure_at = departure_at
+      @results = []
+      setup_capybara
+    end
+
+    def self.find(from, to, departure_at)
+      puts "From: #{from}"
+      puts "To: #{to}"
+      puts "Departure At: #{departure_at}"
+      scraper = new(from, to, departure_at)
+      results = scraper.fetch_results
+    end
+
+    def fetch_results
+      if use_fixture? 
+        fetch_results_from_fixture
+      else
+        fetch_results_live
+      end
+    end
+
+    private
+
+    def fetch_results_live
+      origin = URI.encode_www_form_component("urn:trainline:generic:loc:7527") # London (Any)
+      destination = URI.encode_www_form_component("urn:trainline:generic:loc:4922") # Paris (Any)
+      date = URI.encode_www_form_component("2025-11-20")
+
+      session = Capybara::Session.new(:cuprite)
+
+      begin
+        url = build_url(origin, destination, date)
+        session.visit(url)
+        accept_cookies(session)
+        wait_page_to_load(session)
+      ensure
+        session.driver.quit
+      end
+    end
+
+    def fetch_results_from_fixture
+      
+    end
+
+    def use_fixture?
+      false
+    end
+
+    def build_url(origin, destination, date)
+      "#{BASE_URL}/book/results?origin=#{origin}&destination=#{destination}&outwardDate=#{date}"
+    end
+
+    def accept_cookies(session)
+      # 1. Wait for and accept cookies (mandatory!)
+      if session.has_css?('#onetrust-accept-btn-handler', wait: 10)
+        session.find('#onetrust-accept-btn-handler').click
+      end
+    end
+
+    def wait_page_to_load(session)
+      # 2. Wait for CAPTCHA resolution (you do this manually)
+      puts "Solve captcha if needed..."
+      sleep 2 # or wait until captcha disappears
+
+      # 3. Wait for journey list hydration
+      puts "Wait for journey list hydration..."
+      session.has_css?('[role="tabpanel"]', wait: 20)
+
+      # 4. Wait for at least one journey row to exist
+      puts "Wait for at least one journey row to exist..."
+      session.has_css?('[data-test*="journey-row"], [id^="result-row-journey-"]', wait: 20)
+
+      # Let Trainline finish last DOM updates
+      session.has_no_css?('[data-test="loading"]', wait: 15) rescue nil
+      sleep 1.5
+    end
+
+    def setup_capybara
+      Capybara.default_driver = :cuprite
+      Capybara.default_max_wait_time = 20
+
+      Capybara.register_driver(:cuprite) do |app|
+        Capybara::Cuprite::Driver.new(
+          app,
+          headless: false,
+          window_size: [1280, 900],
+          timeout: 30,
+          process_timeout: 40,
+          js_errors: false
+        )
+      end
     end
 
     # frozen_string_literal: true
--- main.rb
+++ main.rb
@@ -8,65 +8,18 @@ require 'debug'
 
 puts 'This is the beginning of the program'
 
+
 # * The bot should respond to `Scraper::Thetrainline.find(from, to, departure_at)`
+scraper = Scraper::TheTrainline.find('London', 'Berlin', DateTime.now)
 
-Capybara.default_driver = :cuprite
-Capybara.default_max_wait_time = 20
-
-Capybara.register_driver(:cuprite) do |app|
-  Capybara::Cuprite::Driver.new(
-    app,
-    headless: false,
-    window_size: [1280, 900],
-    timeout: 30,
-    process_timeout: 40,
-    js_errors: false
-  )
-end
-
-session = Capybara::Session.new(:cuprite)
-
-origin = URI.encode_www_form_component("urn:trainline:generic:loc:7527") # London (Any)
-destination = URI.encode_www_form_component("urn:trainline:generic:loc:4922") # Paris (Any)
-date = URI.encode_www_form_component("2025-11-20")
-url ="https://www.thetrainline.com/book/results?origin=#{origin}&destination=#{destination}&outwardDate=#{date}"
-
-begin
-  session.visit(url)
-
-  # 1. Wait for and accept cookies (mandatory!)
-  if session.has_css?('#onetrust-accept-btn-handler', wait: 10)
-    session.find('#onetrust-accept-btn-handler').click
-  end
-
-  # 2. Wait for CAPTCHA resolution (you do this manually)
-  puts "Solve captcha if needed..."
-  sleep 2 # or wait until captcha disappears
+pp scraper
 
 
-  # 3. Wait for journey list hydration
-  puts "Wait for journey list hydration..."
-  session.has_css?('[role="tabpanel"]', wait: 20)
 
-  # 4. Wait for at least one journey row to exist
-  puts "Wait for at least one journey row to exist..."
-  session.has_css?('[data-test*="journey-row"], [id^="result-row-journey-"]', wait: 20)
-
-  # Let Trainline finish last DOM updates
-  session.has_no_css?('[data-test="loading"]', wait: 15) rescue nil
-  sleep 1.5
-
-  # 5. ONLY NOW call the parser
-  puts "Only now call the parser..."
-
-#  scraper = Scraper::TheTrainline.new('London', 'Paris', DateTime.now)
-  fixture = Scraper::TheTrainline::Fixture.new(from = 'London', to = 'Paris').save_fixture(session.html) 
+# scraper = Scraper::TheTrainline.new('London', 'Paris', DateTime.now)
+# fixture = Scraper::TheTrainline::Fixture.new(from = 'London', to = 'Paris').save_fixture(session.html) 
 # Now parse
-  results = Scraper::TheTrainline::Parser.new(session).parse
-  pp results
-ensure
-  session.driver.quit
-end
+# results = Scraper::TheTrainline::Parser.new(session).parse
 
 # Scraper::TheTrainline.find('London', 'Paris', DateTime.now)
 
